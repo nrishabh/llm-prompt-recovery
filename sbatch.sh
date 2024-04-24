9d1717da025cd4e704a2f874300a11ea0960ee55
Kunal <kunalkotkar28@gmail.com>
2024-04-24 00:07:23 -0400

    cleaned files

---
+++ sbatch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=mig_class
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=4:00:0
+#SBATCH --job-name="ssm-project-prompt-recovery-baseline"
+# save output in slurm_outputs directory
+#SBATCH --output=slurm_outputs/slurm-%j.out
+#SBATCH --mem=16G
+
+module load anaconda
+conda activate ssm_project
+
+python baseline.py
